fdc57af99e587268f8353ca1c2cf8bfbe928b8ff
Leo van Rensburg <leo.van.rensburg@gmail.com>
2024-07-16 10:47:11 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,9 @@
 # 1 - Mesh Network Topology
 # 2 - Star Network Topology
 # 3 - Tree Network Topology
-# 4 - Hierarchical Network Topology
-# 5 - Hierarchical Network Topology
-# 6 - HLF Distributed Topology
-# 7 - Mesh Vector Topology
-# 8 - Star Vector Topology
+# 4 - HLF Distributed Topology
+# 5 - Mesh Vector Topology
+# 6 - Star Vector Topology
 TOPOLOGY_CHOICE=1
 
 # Function to get the current filename
@@ -24,6 +22,7 @@ mesh_topology() {
     logPath="/home/whoami/Documents/logs/$node/log.txt"
     python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
+
 mesh_vector_topology() {
     get_filename
     pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/meshV/main$node.py"
@@ -31,7 +30,6 @@ mesh_vector_topology() {
     python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
 	
-
 # Function for executing the script for Star Network Topology
 star_topology() {
     get_filename
@@ -39,13 +37,13 @@ star_topology() {
     logPath="/home/whoami/Documents/logs/$node/log.txt"
     python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
+
 star_vector_topology() {
     get_filename
     pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/starV/main$node.py"
     logPath="/home/whoami/Documents/logs/$node/log.txt"
     python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
-
 
 # Function for executing the script for Tree Network Topology
 tree_topology() {
@@ -54,18 +52,7 @@ tree_topology() {
     logPath="/home/whoami/Documents/logs/$node/log.txt"
     python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
-hlf_mesh() {
-    get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/hlf-mesh/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
-}
-hlf_proper() {
-    get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/hlf/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
-}
+
 hlf_distributed() {
     get_filename
     pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/hlf-distributed/main$node.py"
@@ -78,10 +65,8 @@ case $TOPOLOGY_CHOICE in
     1) mesh_topology ;;
     2) star_topology ;;
     3) tree_topology ;;
-    4) hlf_mesh ;;
-    5) hlf_proper ;;
-    6) hlf_distributed ;;
-    7) mesh_vector_topology ;;
-    8) star_vector_topology ;;
+    4) hlf_distributed ;;
+    5) mesh_vector_topology ;;
+    6) star_vector_topology ;;
     *) echo "Invalid topology choice"; exit 1 ;;
 esac
