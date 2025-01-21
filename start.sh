d514d2f4474470dc125d356f78efb0c8ba25cd3a
Leo van Rensburg <leo.van.rensburg@gmail.com>
2025-01-21 22:17:22 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,7 @@
 # 2 - Star Network Topology
 # 3 - Tree Network Topology
 # 4 - HLF Distributed Topology
-# 5 - Mesh Vector Topology
-# 6 - Star Vector Topology
-TOPOLOGY_CHOICE=1
+TOPOLOGY_CHOICE=4
 
 # Function to get the current filename
 get_filename() {
@@ -18,46 +16,32 @@ get_filename() {
 # Function for executing the script for Mesh Network Topology
 mesh_topology() {
     get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/mesh/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
-}
-
-mesh_vector_topology() {
-    get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/meshV/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
+    pythonScriptPath="/home/leo/Documents/DistributedConsensusAlgorithm/Algorithms/consensus_node.py"
+    logPath="/home/leo/Documents/logs/$node/log.txt"
+    python3 -u "$pythonScriptPath" mesh $node 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
 	
 # Function for executing the script for Star Network Topology
 star_topology() {
     get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/star/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
-}
-
-star_vector_topology() {
-    get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/starV/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
+    pythonScriptPath="/home/leo/Documents/DistributedConsensusAlgorithm/Algorithms/consensus_node.py"
+    logPath="/home/leo/Documents/logs/$node/log.txt"
+    python3 -u "$pythonScriptPath" star $node 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
 
 # Function for executing the script for Tree Network Topology
 tree_topology() {
     get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/tree/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
+    pythonScriptPath="/home/leo/Documents/DistributedConsensusAlgorithm/Algorithms/consensus_node.py"
+    logPath="/home/leo/Documents/logs/$node/log.txt"
+    python3 -u "$pythonScriptPath" tree $node 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
 
 hlf_distributed() {
     get_filename
-    pythonScriptPath="/home/whoami/Documents/DistributedConsensusAlgorithm/Algorithms/hlf-distributed/main$node.py"
-    logPath="/home/whoami/Documents/logs/$node/log.txt"
-    python3 -u "$pythonScriptPath" 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
+    pythonScriptPath="/home/leo/Documents/DistributedConsensusAlgorithm/Algorithms/consensus_node.py"
+    logPath="/home/leo/Documents/logs/$node/log.txt"
+    python3 -u "$pythonScriptPath" hlf $node 2>&1 | while IFS= read -r line; do echo "$(date +"[%Y-%m-%d %H:%M:%S.%3N]") $line"; done > "$logPath"
 }
 
 # Decide which function to call based on topology choice
@@ -66,7 +50,5 @@ case $TOPOLOGY_CHOICE in
     2) star_topology ;;
     3) tree_topology ;;
     4) hlf_distributed ;;
-    5) mesh_vector_topology ;;
-    6) star_vector_topology ;;
-    *) echo "Invalid topology choice"; exit 1 ;;
+    *) echo "Invalid topology choice!@"; exit 1 ;;
 esac
